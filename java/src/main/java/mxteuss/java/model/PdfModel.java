ff49436bbc9bba39d1f737ac4e59cc4792d2ecbd
mxteuss <ovagabundant@gmail.com>
2026-02-06 02:07:03 -0300

    feat: propriedades pdf

--- java/src/main/java/mxteuss/java/model/PdfModel.java
+++ java/src/main/java/mxteuss/java/model/PdfModel.java
@@ -10,4 +10,15 @@ public class PdfModel {
     private String instituicao;
     private String ano;
     private String cidade;
+    private String orientador;
+    private String errata;
+    private String dedicatoria;
+    private String agradecimentos;
+    private String epigrafe;
+    private String resumo;
+    private String palavrasChave;
+    private String resumoEn;
+    private String keywords;
+    private String tipoTrabalho;
+    private String objetivo;
 }
