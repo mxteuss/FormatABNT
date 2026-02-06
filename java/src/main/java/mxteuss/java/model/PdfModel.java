eaf2cc055f5937bdd3226d01f9c26314a7ee467d
mxteuss <ovagabundant@gmail.com>
2026-02-05 21:53:40 -0300

    feat: pdf

---
+++ java/src/main/java/mxteuss/java/model/PdfModel.java
@@ -0,0 +1,13 @@
+package mxteuss.java.model;
+
+import lombok.Data;
+
+@Data
+public class PdfModel {
+    private String curso;
+    private String titulo;
+    private String nome;
+    private String instituicao;
+    private String ano;
+    private String cidade;
+}
